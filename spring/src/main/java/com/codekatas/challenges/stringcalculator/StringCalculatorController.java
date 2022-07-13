87693def35060a860f8e53c8c121b04e4bc4aaa3
sidyes <s10h@posteo.de>
2022-07-13 07:19:24 +0200

    finalize java katas

--- spring/src/main/java/com/codekatas/challenges/stringcalculator/StringCalculatorController.java
+++ spring/src/main/java/com/codekatas/challenges/stringcalculator/StringCalculatorController.java
@@ -1,2 +1,22 @@
-package com.codekatas.challenges.stringcalculator;public class StringCalculatorController {
+package com.codekatas.challenges.stringcalculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StringCalculatorController {
+
+  @Autowired
+  private StringCalculatorService stringCalculatorService;
+
+  @GetMapping(value = "/string-calculator")
+  @ResponseBody
+  public String getCalculatedString(@RequestParam("input") String input) {
+
+    // TODO: use stringCalculatorService
+    return "UNKNOWN";
+  }
 }
